c274fc4c2634a713b3f3089a37c5281d51de17ff
DESKTOP-LBOHS1J\hungnm2 <hung.nguyen@netvisiontel.com>
2020-03-23 18:31:43 +0700

    update

--- src/main/java/hung/com/vertx/package-info.java
+++ src/main/java/hung/com/vertx/package-info.java
@@ -1,4 +1,4 @@
-@ModuleGen(groupPackage = "hung.com", name = "vertx")
+@ModuleGen(groupPackage = "hung.com.vertx", name = "generated")
 package hung.com.vertx;
 
 import io.vertx.codegen.annotations.ModuleGen;
